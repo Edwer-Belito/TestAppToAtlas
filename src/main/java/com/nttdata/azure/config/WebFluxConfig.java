dcfc89d47bab2546139d8459cfc858cea64c78f7
Edwer Belito <ebelitoa@emeal.nttdata.com>
2022-03-17 13:52:03 -0500

    webflux config

---
+++ src/main/java/com/nttdata/azure/config/WebFluxConfig.java
@@ -0,0 +1,17 @@
+package com.nttdata.azure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * anotacion configuracion.
+ */
+@Configuration
+/**
+ * abilita app reactiva.
+ */
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
+
+}
